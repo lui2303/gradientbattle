f478df42086ac95070af5c5e1edd07dff3bbd931
Luis Wiederhold <wiederhold.luis@gmx.de>
2026-05-01 00:46:45 +0000

    added dynamic leaderboard

--- apps/web/app/components/Leaderboard.tsx
+++ apps/web/app/components/Leaderboard.tsx
@@ -4,7 +4,7 @@ import { Optimizer } from "../types";
 import { norm } from "@gradientbattle/core/src/math_helper"
 
 export function Leaderboard({optimizers, optimizerTraces}: {optimizers: Record<string, Optimizer>, optimizerTraces: Partial<PlotData>[]}) {
-
+    
     return (
         <div>
             <table>
@@ -16,15 +16,16 @@ export function Leaderboard({optimizers, optimizerTraces}: {optimizers: Record<s
                     </tr>
                 </thead>
                 <tbody>{optimizerTraces.length !== 0 ? 
-                    optimizerTraces.map((optimizerTrace) => {
+                    optimizerTraces.toSorted((a, b) => norm({x: a.x![a.x!.length - 1] as number, y: a.y![a.y!.length - 1] as number}) - norm({x: b.x![b.x!.length - 1] as number, y: b.y![b.y!.length - 1] as number})).map((optimizerTrace,i) => {
                         const traceLength = optimizerTrace.x!.length // could be moved up because it is same for all traces
                         return (
                             <tr style={{ backgroundColor: optimizers[optimizerTrace.name as string].color }} key={optimizerTrace.name}>
+                                <td>{i + 1}</td>
                                 <td>{optimizers[optimizerTrace.name as string].name}</td>
                                 <td>{norm({x: optimizerTrace.x![traceLength - 1] as number, y: optimizerTrace.y![traceLength - 1] as number})}</td>
                             </tr>
                         )
-                    }) : Object.keys(optimizers).map((optiKey, i) => {
+                    }) : Object.keys(optimizers).toSorted((a,b ) => norm(optimizers[a].startingPoint) -norm(optimizers[b].startingPoint)).map((optiKey, i) => {
                         return (
                             <tr style={{ backgroundColor: optimizers[optiKey].color}} key={optiKey}>
                                 <td>{i + 1}</td>
